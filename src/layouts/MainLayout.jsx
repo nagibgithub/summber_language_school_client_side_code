1cffff8a0382df10eb5a7813186a4123acadc00f
Nagib Mahfuz <120543584+nagibgithub@users.noreply.github.com>
2023-06-08 23:11:12 +0600

    Login and reg page

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,9 +4,11 @@ import Footer from "../shared/Footer";
 
 const MainLayout = () => {
     return (
-        <div className="min-h-screen justify-between flex flex-col">
-            <NavBar></NavBar>
-            <Outlet></Outlet>
+        <div className="min-h-screen justify-between flex flex-col container mx-auto">
+            <div>
+                <NavBar></NavBar>
+                <Outlet></Outlet>
+            </div>
             <Footer></Footer>
         </div>
     );
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,17 +1,18 @@
 
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
-import App from "../App";
+import Home from "../pages/Home/Home/Home";
+import Login from "../pages/login_register/Login";
+import Register from "../pages/login_register/Register";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <MainLayout />,
         children: [
-            {
-                path: "/",
-                element: <App></App>
-            }
+            { path: "/", element: <Home /> },
+            { path: "/login", element: <Login /> },
+            { path: "/register", element: <Register /> },
         ]
     },
 ]);
